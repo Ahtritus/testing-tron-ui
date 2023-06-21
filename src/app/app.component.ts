dfc5e5fc0f31c097a0277990eccca08e877ee399
Sutirtha <sdas@republicfinance.com>
2023-06-21 20:06:41 +0530

    BLD: Added configs for using tailwind styles from library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  ngOnInit() {
+    // this.themeService.setTheme('dark'); // Set the desired theme name here
+  }
+
   title = 'myapp';
 }
